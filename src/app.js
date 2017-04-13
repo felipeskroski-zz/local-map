80d58a2a70d8130e8669a6e0b981051fc7f3553e
felipeskroski <felipeskroski@gmail.com>
2017-04-13 22:34:19 +1200

    select items from list

--- src/app.js
+++ src/app.js
@@ -1,2 +1,106 @@
 import points from './points.js'
-import './list.js'
+function LocationsViewModel() {
+    var self = this;
+    // add points to model
+    self.points = ko.observable(points);
+    self.filter = ko.observable()
+
+    // filters list based on search box
+    self.filter.subscribe(function(value) {
+      let filtered = points.filter(function(point){
+        if(point.title.toLowerCase().includes(value.toLowerCase()) ){
+          return point
+        }
+      })
+      self.points(filtered)
+      renderPoints(filtered)
+    });
+
+    self.item_select = function(item){
+      return select_marker_from_list(item.id)
+    }
+
+
+}
+ko.applyBindings(new LocationsViewModel());
+
+
+// Create a map variable
+let map;
+let markers = [];
+// Function to initialize the map within the map div
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat:-41.291697, lng:174.783407},
+    zoom:14,
+    maxZoom: 14,
+  });
+  // Create a single latLng literal object.
+  renderPoints(points)
+
+}
+function clearPoints(){
+  markers.map(m => {
+    return m.setMap(null)
+  })
+  markers = [];
+}
+
+function select_marker_from_list(id){
+  let point = get_marker_by_id(id)
+  return toggleBounce(point)
+}
+
+function get_marker_by_id(id){
+  let point = markers.find(m => {
+    console.log(m.id)
+    if(m.id == id){
+      return m
+    }
+  })
+  return point
+}
+
+function renderPoints(points){
+  clearPoints()
+  // iterate over points and create all markers
+  let bubble = new google.maps.InfoWindow()
+  let bounds = new google.maps.LatLngBounds();
+  markers = points.map(p => {
+    const marker = new google.maps.Marker({
+      position: p.pos,
+      map: map,
+      animation: google.maps.Animation.DROP,
+      title: p.title,
+      id: p.id
+    });
+    bounds.extend(p.pos)
+    marker.addListener('click', function() {
+      populateInfoWindow(this, bubble);
+      this.setAnimation(null)
+    });
+    return marker
+  })
+  map.fitBounds(bounds)
+  // Opens the infowindow
+}
+
+function toggleBounce(m) {
+  markers.map(point => {
+    return point.setAnimation(null)
+  })
+  m.setAnimation(google.maps.Animation.BOUNCE);
+}
+
+function populateInfoWindow(point, infowindow){
+  if(infowindow.marker != point){
+    infowindow.marker = point;
+    infowindow.setContent('<div>' + point.title + '</div>');
+    infowindow.open(map, point);
+    infowindow.addListener('closeclick', () =>{
+      infowindow.marker = null;
+    });
+  }
+}
+
+window.initMap = initMap;
--- src/points.js
+++ src/points.js
@@ -1,6 +1,9 @@
 const points = [
-  {title: 'Lyall Bay', pos:{lat:-41.291697, lng:174.783407}},
-  {title: 'Houghton Bay', pos:{lat:-41.342761, lng:174.784853}},
-  {title: 'Breaker bay', pos:{lat:-41.332655, lng:174.831025}}
+  {id: 1, title: 'Lyall Bay', pos:{lat:-41.329401, lng:174.804075}},
+  {id: 2, title: 'Houghton Bay', pos:{lat:-41.342761, lng:174.784853}},
+  {id: 3, title: 'Breaker Bay', pos:{lat:-41.332655, lng:174.831025}},
+  {id: 4, title: 'The Island', pos:{lat:-41.349890, lng:174.775681}},
+  {id: 5, title: 'Airport Rights', pos:{lat:-41.337035, lng:174.807642}},
+  {id: 6, title: 'Propellers', pos:{lat:-41.343915, lng:174.822018}},
 ]
 export default points;
